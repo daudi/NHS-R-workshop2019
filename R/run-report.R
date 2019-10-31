de87732bdb59e05deaaa1b7cdc6f6481d0dc2b52
David Whiting <david.whiting@publichealth.me.uk>
2019-10-31 14:29:18 +0000

    Step10 - create word output

--- R/run-report.R
+++ R/run-report.R
@@ -1,25 +1,15 @@
 ## Script to run an Rmd file multiple times
 ##
-## Rather than having to specify the names of the areas we want to run 
-## this for, we can get the names from the data file. We can then use 
-## these area names in a for loop or create a function and use lapply()
+## So far we have been creating HTML output. But we can also create
+## word documents by specifying the output_format. Make sure you also
+## change the extension in the output file name.
 
 library(fingertipsR)
 
-
 if (!exists("LEdata")) LEdata <- fingertips_data(90366, AreaTypeID = 101)
-
-## Have a look at the data set
-head(LEdata)
-
-## See what area types we have 
-table(LEdata$AreaType)
 
 ## Select areas that are districts
 areas <- unique(LEdata$AreaName[LEdata$AreaType == "District & UA (pre 4/19)"])
-
-## Have a look at the first few district names
-head(areas)
 
 ## Limit to the first five areas (to save time for now)
 areas <- areas[1:5]
@@ -27,22 +17,12 @@ areas <- areas[1:5]
 ## Create the output folder if it does not already exist
 if (!file.exists("../output")) dir.create("../output")
 
-## Produce the reports using a for loop
-for (i in 1:length(areas)) {
-  this_area <- areas[i]
-  outfile <- paste0("../output/", this_area, ".html")
-  rmarkdown::render("district-report.Rmd", 
-                  params = list(area = this_area),
-                  output_file = outfile)
-}
- 
-
-## Produce the reports using lapply()
-
+# Produce the reports using lapply()
 produce_report <- function(this_area) {
-  outfile <- paste0("../output/", this_area, ".html")
-  rmarkdown::render("district-report.Rmd", 
+  outfile <- paste0("../output/", this_area, ".docx")
+  rmarkdown::render("district-report.Rmd",
                     params = list(area = this_area),
+                    output_format = "word_document", # Word format
                     output_file = outfile)
 }
 
