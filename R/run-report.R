a9404ba987a0516cc3776fbada073008c90da8a9
David Whiting <david.whiting@publichealth.me.uk>
2019-10-31 14:29:17 +0000

    Step07 - run for three areas

--- R/run-report.R
+++ R/run-report.R
@@ -1,12 +1,26 @@
-## Script to run an Rmd file
+## Script to run an Rmd file for three different areas
 ##
-## In this script we tell render() the parameter value we want to use
-## by supplying a list.
-##
-## By default the output file name will be district-report.html
-## 
-## Note that by default this runs in your current environment, whereas using 
-## the Knit menu runs the Rmd file in it's own environment. This means that 
-## you Rmd file can modify or over-write any objects in your working enviroment.
+## Now we call render() three times with different areas each time. 
+## We also specify the name of the output file. We could specify 
+## the output_dir and not the file name, but this would call each
+## report district-report.Rmd so each subsequent report would 
+## over-write the previous one.
 
-rmarkdown::render("district-report.Rmd", params = list(area = "Medway"))
+
+rmarkdown::render("district-report.Rmd", 
+                  params = list(area = "Medway"),
+                  output_file = "Medway.html")
+
+rmarkdown::render("district-report.Rmd", 
+                  params = list(area = "Birmingham"),
+                  output_file = "Birmingham.html")
+
+rmarkdown::render("district-report.Rmd", 
+                  params = list(area = "Leicester"),
+                  output_file = "Leicester.html")
+
+
+
+## One problem with this approach is that it calls fingertips_data() each time
+## and therefore will be slow when we need to run it for lots of areas.
+## We can improve this by changing our Rmd file slightly.
