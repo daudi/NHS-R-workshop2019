83fd76dc9dbf6d2afe62219a855df8d23e94f648
David Whiting <david.whiting@publichealth.me.uk>
2019-10-31 14:29:18 +0000

    Step12 - run for 326 districts and unitary autorities

--- R/run-report.R
+++ R/run-report.R
@@ -1,14 +1,10 @@
 ## Script to run an Rmd file multiple times
 ##
-## There are various options that you can set to control the 
-## structure and appearance of your document. You can specify 
-## these options in the YAML section of the Rmd file, and you 
-## can also specify them in the output_format option when calling
-## render(). Use html_document() or word_document() to do this.
-## See their respective help files.
-##
-## In this example we include a table of contents.
+## Now produce a profile for all 326 districts and local authorities 
+## in the country. It takes about 6 minutes on my laptop.
+ 
 
+t1 <- Sys.time()
 
 library(fingertipsR)
 
@@ -18,9 +14,6 @@ if (!exists("LEdata")) LEdata <- fingertips_data(90366, AreaTypeID = 101)
 ## Select areas that are districts
 areas <- unique(LEdata$AreaName[LEdata$AreaType == "District & UA (pre 4/19)"])
 
-## Limit to the first five areas (to save time while testing)
-areas <- areas[1:5]
-
 ## Create the output folder if it does not already exist
 if (!file.exists("../output")) dir.create("../output")
 
@@ -28,21 +21,14 @@ if (!file.exists("../output")) dir.create("../output")
 for (i in 1:length(areas)) {
   this_area <- areas[i]
   outfile <- paste0("../output/", this_area, ".html")
-  rmarkdown::render("district-report-5.Rmd", 
+  rmarkdown::render("district-report.Rmd", 
                   params = list(area = this_area),
                   output_format = rmarkdown::html_document(toc = TRUE),
                   output_file = outfile)
 }
  
 
-## Produce the reports
-for (i in 1:length(areas)) {
-  this_area <- areas[i]
-  outfile <- paste0("../output/", this_area, ".docx")
-  rmarkdown::render("district-report.Rmd", 
-                  params = list(area = this_area),
-                  output_format = rmarkdown::word_document(toc = TRUE),
-                  output_file = outfile)
-}
- 
 
+t2 <- Sys.time()
+
+duration <- t2 - t1 # This took 6 minutes on my machine.
