f65603730f474944eca5d2ab2e7c94e81431037f
David Whiting <david.whiting@publichealth.me.uk>
2019-10-31 14:29:17 +0000

    Step06 - render with parameters

--- R/run-report.R
+++ R/run-report.R
@@ -1,6 +1,12 @@
 ## Script to run an Rmd file
 ##
-## Note that this uses the default parameters in the Rmd file
+## In this script we tell render() the parameter value we want to use
+## by supplying a list.
+##
+## By default the output file name will be district-report.html
+## 
+## Note that by default this runs in your current environment, whereas using 
+## the Knit menu runs the Rmd file in it's own environment. This means that 
+## you Rmd file can modify or over-write any objects in your working enviroment.
 
-
-rmarkdown::render("district-report.Rmd")
+rmarkdown::render("district-report.Rmd", params = list(area = "Medway"))
